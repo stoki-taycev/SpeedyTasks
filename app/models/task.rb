86fe36c1b2bec4a679653128aeee46e2978a71a6
Anton Udyrenko <stokipost@gmail.com>
2015-02-23 02:39:41 +0200

    Bug with before action user check fixed. No users duplicates while sharing.

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,19 +8,5 @@ class Task < ActiveRecord::Base
     self.users << new_user
   end
 
-  def add_new_user new_user
-    if find_user_by_email
-      self.users << new_user
-    end
-  end
-
-  def find_user_by_email email_address
-    self.users.each do |user|
-      if user.find_by(:email => email_address)
-        return false
-      end
-    end
-  end
-
 end
 
