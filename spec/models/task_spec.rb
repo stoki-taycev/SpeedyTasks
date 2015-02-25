39d8b9994d7ad8b7239c50fd745ecb9f0f8a295b
Anton Udyrenko <stokipost@gmail.com>
2015-02-25 15:01:00 +0200

    RSpec all Task model tests passed.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
 
+  it "should have a Title to be created" do
+    @task = Task.create(:title => '')
+    @task.valid?
+    expect(@task.errors).to have_key(:title)
+  end
+
+  it "should retrieve users from database as an Array" do
+    @task = Task.create(:title => 'Shared task', :owners => ['batman', 'superman', 'spiderman'])
+    @task.reload
+    expect(@task.owners.kind_of?(Array)).to eq(true)
+  end
+
   it "should have many users" do
     @task = Task.create(:title => 'RSpec Task1')
     @user1 = User.create(:email => 'user1@user.com', :password => '12345678')
@@ -19,7 +31,6 @@ RSpec.describe Task, :type => :model do
     expect(@user1.tasks.first).to eq(@task)
     expect(@user2.tasks.first).to eq(@task)
     expect(@user3.tasks.first).to eq(@task)
-
   end
 
 
